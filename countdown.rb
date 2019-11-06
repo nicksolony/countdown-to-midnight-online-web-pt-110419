a0462d4231cfd52f58dcc641bce5a6edbc9c48ed
Mykola Solonyy <nicksolony@gmail.com>
2019-11-06 05:40:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number>0
+  
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  
+  end
+  
+  "HAPPY NEW YEAR!"
+
+end
+
+#write your code here
+
+def countdown_with_sleep(number)
+  while number>0
+  
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(1)
+
+  end
+  
+  "HAPPY NEW YEAR!"
+
 end
