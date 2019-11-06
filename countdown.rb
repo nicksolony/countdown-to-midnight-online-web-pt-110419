5b2068d250548f60b986e43d31af69c366085182
Mykola Solonyy <nicksolony@gmail.com>
2019-11-06 05:39:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,17 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 
 end
+
+#write your code here
+
+def countdown(number)
+  while number>0
+  
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  
+  end
+  
+  "HAPPY NEW YEAR!"
+
+end
